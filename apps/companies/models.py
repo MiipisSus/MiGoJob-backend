b911c0296afdd20197e5f25e56fe04fa1771c5b8
MiipisSus <kokurui0113@gmail.com>
2025-06-11 20:01:40 +0800

    refactor: Adjust average_salary property code.

---
+++ apps/companies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db.models import F, ExpressionWrapper
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    
+    @property
+    def average_salary(self):
+        jobs = self.jobs.annotate(
+            midpoint=ExpressionWrapper((F('salary_min') + F('salary_max')) / 2, output_field=models.FloatField())
+        )
+        avg = jobs.aggregate(avg=models.Avg('midpoint'))['avg']
+        return round(avg, 2) if avg is not None else 0
+    
+    class Meta:
+        db_table = 'companies'
