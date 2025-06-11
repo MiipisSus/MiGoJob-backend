5222ae8253921740853e42415a88afd24de1b0d4
MiipisSus <kokurui0113@gmail.com>
2025-06-11 17:24:58 +0800

    feat: Add the jobs router and companies router to the api.

--- MiGoJob/api.py
+++ MiGoJob/api.py
@@ -1,4 +1,11 @@
 from ninja import NinjaAPI
+from ninja.errors import ValidationError
+from django.http import HttpResponse
 
+from companies.api import router as companies_router
+from jobs.api import router as jobs_router
 
 api = NinjaAPI()
+
+api.add_router('/companies/', companies_router)
+api.add_router('/jobs/', jobs_router)
