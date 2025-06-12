d73f139375592a4280db4b7a088d6d4ca37bd1c4
MiipisSus <kokurui0113@gmail.com>
2025-06-12 19:10:14 +0800

    feat: Add `company_name` to JobSchema. refactor: Rename `JobSchema` to `JobResponseSchema`, the original JobSchema changed to be a base class. refactor: Rename `company` field to `company_id`.

--- apps/jobs/schemas.py
+++ apps/jobs/schemas.py
@@ -1,7 +1,21 @@
 from .models import Job
 from ninja import ModelSchema
 
+
 class JobSchema(ModelSchema):
+    company_id: int
+    
+    @staticmethod
+    def resolve_company_id(obj):
+        return obj.company.id
+    
     class Meta:
         model = Job
-        fields = '__all__'
+        exclude = ['company']
+        
+class JobResponseSchema(JobSchema):
+    company_name: str
+    
+    @staticmethod
+    def resolve_company_name(obj):
+        return obj.company.name
