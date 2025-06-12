1ca1f5c8a103ac502312787fcc33ffce208c5f40
MiipisSus <kokurui0113@gmail.com>
2025-06-12 19:11:10 +0800

    feat: Add `high_salary_jobs` field to CompanySchema.

--- apps/companies/schemas.py
+++ apps/companies/schemas.py
@@ -1,9 +1,18 @@
 from ninja import ModelSchema
+from typing import List
 
 from .models import Company
+from apps.jobs.schemas import JobSchema
+
 
 class CompanySchema(ModelSchema):
+    jobs: List[JobSchema]
+    high_salary_jobs: List[JobSchema]
     average_salary: float
+    
+    @staticmethod
+    def resolve_highest_salary_jobs(obj):
+        return obj.jobs.filter(salary_min__gte=100000).order_by('-salary_min')
     
     class Meta:
         model = Company
