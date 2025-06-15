496c4d469b308411b6d4954f3d700e4721a6684a
MiipisSus <kokurui0113@gmail.com>
2025-06-15 21:03:24 +0800

    feat: Refactor job API to improve response schemas and error handling.

--- apps/jobs/api.py
+++ apps/jobs/api.py
@@ -1,21 +1,42 @@
 from ninja import Router
-from ninja.errors import HttpError
 from typing import List
+from django.shortcuts import get_object_or_404
 
-from .models import Job
-from .schemas import JobSchema, JobResponseSchema
-
+from apps.jobs.models import Job
+from apps.companies.models import Company
+from .schemas import JobIn, JobOut
+from apps.common.shortcuts import update_object
+from apps.auth import IsSuperuser
 
 router = Router(tags=['jobs'])
 
-@router.get('', response=List[JobResponseSchema])
+@router.get('', response=List[JobOut])
 def list_jobs(request, keyword: str = None):
     jobs = Job.objects.filter(name__contains=keyword) if keyword else Job.objects.all()
     return jobs.order_by('-created_at')
 
-@router.get('/{id}', response=JobResponseSchema)
+@router.get('/{id}', response=JobOut)
 def retrieve_job(request, id: int):
-    try:
-        return Job.objects.get(id=id)
-    except Job.DoesNotExist:
-        raise HttpError(404, f'Job with id {id} not found')
+    return get_object_or_404(Job, id=id)
+    
+@router.post('', response=JobOut, auth=IsSuperuser())
+def create_job(request, payload: JobIn):
+    get_object_or_404(Company, id=payload.company_id)
+    
+    job = Job.objects.create(**payload.dict())
+    return job
+
+@router.patch('/{id}', response=JobOut, auth=IsSuperuser())
+def update_job(request, id: int, payload: JobIn):
+    get_object_or_404(Company, id=payload.company_id)
+    
+    job = get_object_or_404(Job, id=id)
+    job = update_object(instance=job, payload=payload)
+    
+    return job
+
+@router.delete('/{id}', auth=IsSuperuser())
+def delete_job(request, id: int):
+    job = get_object_or_404(Job, id=id)
+    job.delete()
+    return
