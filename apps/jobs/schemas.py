397e58f6123a321626cca5ded7e088a16a41228f
MiipisSus <kokurui0113@gmail.com>
2025-06-15 21:03:36 +0800

    feat: Refactor Job schemas to improve structure and resolve methods

--- apps/jobs/schemas.py
+++ apps/jobs/schemas.py
@@ -1,21 +1,27 @@
 from .models import Job
 from ninja import ModelSchema
 
-
-class JobSchema(ModelSchema):
+        
+class JobIn(ModelSchema):
     company_id: int
     
+    class Meta:
+        model = Job
+        exclude = ['id', 'company', 'created_at', 'updated_at']
+        
+class JobOut(ModelSchema):
+    company_id: int
+    company_name: str
+    
+    class Meta:
+        model = Job
+        exclude = ['company', 'created_at', 'updated_at']
+
     @staticmethod
     def resolve_company_id(obj):
         return obj.company.id
     
-    class Meta:
-        model = Job
-        exclude = ['company']
-        
-class JobResponseSchema(JobSchema):
-    company_name: str
-    
     @staticmethod
     def resolve_company_name(obj):
         return obj.company.name
+    
